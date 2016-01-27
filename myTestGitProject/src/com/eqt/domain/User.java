16844b499c3df7bdef4cb0e1449c2041e7e72df7
jroberts969 <j_droberts@yahoo.co.uk>
2016-01-27 22:16:45 +0000

    first commit

---
+++ myTestGitProject/src/com/eqt/domain/User.java
@@ -0,0 +1,29 @@
+package com.eqt.domain;
+
+public class User {
+	
+	private String username = "";
+	private String email = "";
+	private Address address = null;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+			
+
+}
