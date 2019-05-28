449fb592d382bc8f215e54e57a2d15fc5bad7e86
Jonathan Roberts <j_droberts@yahoo.co.uk>
2019-05-28 23:30:40 +0100

    added params

--- myTestGitProject/src/com/eqt/domain/User.java
+++ myTestGitProject/src/com/eqt/domain/User.java
@@ -4,8 +4,15 @@ public class User {
 	
 	private String username = "";
 	private String email = "";
+	private String description = "";
 	
 	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	private Address address = null;
 	
 	public String getUsername() {
@@ -26,6 +33,13 @@ public class User {
 	public void setAddress(Address address) {
 		this.address = address;
 	}
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", email=" + email + ", description=" + description + ", address="
+				+ address + "]";
+	}
+	
+	
 			
 
 }
